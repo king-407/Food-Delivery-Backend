47363870481dbb76dc9a1acf8e36a7297ba899d8
king-407 <shivamtiwaritiwari0704@gmail.com>
2023-12-12 23:38:11 +0530

    Order Route added

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,15 +1,31 @@
 const Cart = require("../model/cart");
+const Order = require("../model/order");
 const placeOrder = async (req, res, next) => {
   const { cartId } = req.body;
   try {
-    const cart = await Cart.findOne({ _id: cartId }).populate("productInfo");
+    const cart = await Cart.findOne({ owner: req.user.userId }).populate({
+      path: "productInfo",
+      populate: {
+        path: "item",
+        model: "Product",
+      },
+    });
     if (!cart) {
       return res.status(400).json({ message: "Cart not found for the user" });
     }
+
     const totalPrice = cart.productInfo.reduce((total, product) => {
-      return total + product.cost * product.quantity;
+      return total + product.item.cost * product.quantity;
     }, 0);
-    console.log(totalPrice);
-  } catch (e) {}
+    const order = new Order({
+      totalPrice,
+      Info: cartId,
+      owner: req.user.userId,
+    });
+    await order.save();
+    return res.status(200).send(order);
+  } catch (e) {
+    console.log(e);
+  }
 };
 module.exports = { placeOrder };
--- model/order.js
+++ model/order.js
@@ -1,4 +1,4 @@
-const mongoose = require("momgoose");
+const mongoose = require("mongoose");
 const orderSchema = mongoose.Schema({
   Info: {
     type: mongoose.Types.ObjectId,
